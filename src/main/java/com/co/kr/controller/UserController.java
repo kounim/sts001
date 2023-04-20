67a5aa9baf0710e1187a159a32e9d3720afc0f6f
kounim <withway20@gmail.com>
2023-04-21 02:31:16 +0900

    7thWeek

--- src/main/java/com/co/kr/controller/UserController.java
+++ src/main/java/com/co/kr/controller/UserController.java
@@ -11,14 +11,20 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.co.kr.domain.BoardListDomain;
 import com.co.kr.domain.LoginDomain;
 import com.co.kr.service.UploadService;
 import com.co.kr.service.UserService;
 import com.co.kr.util.CommonUtils;
+import com.co.kr.util.Pagination;
 import com.co.kr.vo.LoginVO;
 
 import lombok.extern.slf4j.Slf4j;
@@ -75,5 +81,165 @@ public class UserController {
 		mav.addObject("items", items);
 		mav.setViewName("board/boardList.html");
 		return mav; 
+	};
+	
+	@GetMapping("mbList")
+	public ModelAndView mbList(HttpServletRequest request) {
+		
+		ModelAndView mav = new ModelAndView();
+		HttpSession session = request.getSession();
+		String page = (String) session.getAttribute("page");
+		if(page==null)page="1";
+		
+		session.setAttribute("page", page);
+		mav=mbListCall(request);
+		mav.setViewName("admin/adminList.html");
+		return mav;
+	};
+	
+	public ModelAndView mbListCall(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		int totalcount=userService.mbGetAll();
+		int contentnum=10;
+		
+		boolean itemsNotEmpty;
+		if(totalcount>0) {
+			itemsNotEmpty=true;
+			Map<String, Object> pagination=Pagination.pagination(totalcount, request);
+			Map map = new HashMap<String, Integer>();
+			map.put("offset", pagination.get("offset"));
+			map.put("contentnum", contentnum);
+			
+			List<LoginDomain> loginDomain=userService.mbAllList(map);
+			mav.addObject("itemsNotEmpty", itemsNotEmpty);
+			mav.addObject("items", loginDomain);
+			mav.addObject("rowNUM", pagination.get("rowNUM"));
+			mav.addObject("pageNum", pagination.get("pageNum"));
+			mav.addObject("startpage", pagination.get("startpage"));
+			mav.addObject("endpage", pagination.get("endpage"));
+		}else {
+			itemsNotEmpty=false;
+		}
+		return mav;
 	}
+	
+	@GetMapping("/modify/{mbSeq}")
+    public ModelAndView mbModify(@PathVariable("mbSeq") String mbSeq, RedirectAttributes re) throws IOException {
+		ModelAndView mav = new ModelAndView();
+		re.addAttribute("mbSeq", mbSeq);
+		mav.setViewName("redirect:/mbEditList");
+		return mav;
+	};
+	
+	@GetMapping("mbEditList")
+	public ModelAndView mbListEdit(@RequestParam("mbSeq") String mbSeq, HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		mav = mbListCall(request);  
+		Map map = new HashMap<String, String>();
+		map.put("mbSeq", mbSeq);
+		LoginDomain loginDomain = userService.mbSelectList(map);
+		mav.addObject("item",loginDomain);
+		mav.setViewName("admin/adminEditList.html");
+		return mav; 
+	};
+	
+	@RequestMapping("/update")
+	public ModelAndView mbModify(LoginVO loginVO, HttpServletRequest request, RedirectAttributes re) throws IOException {
+		
+		ModelAndView mav = new ModelAndView();
+		HttpSession session = request.getSession();
+		String page = "1";
+		LoginDomain loginDomain = null;
+		
+		String IP = CommonUtils.getClientIP(request);
+		loginDomain = LoginDomain.builder()
+				.mbSeq(Integer.parseInt(loginVO.getSeq()))
+				.mbId(loginVO.getId())
+				.mbPw(loginVO.getPw())
+				.mbLevel(loginVO.getLevel())
+				.mbIp(IP)
+				.mbUse("Y")
+				.build();
+		userService.mbUpdate(loginDomain);
+		re.addAttribute("page",page);
+		mav.setViewName("redirect:/mbList");
+		return mav;
+	};
+	
+	@GetMapping("/remove/{mbSeq}")
+    public ModelAndView mbRemove(@PathVariable("mbSeq") String mbSeq, RedirectAttributes re, HttpServletRequest request) throws IOException {
+		ModelAndView mav = new ModelAndView();
+		Map map = new HashMap<String, String>();
+		map.put("mbSeq", mbSeq);
+		userService.mbRemove(map);
+		HttpSession session = request.getSession();
+		re.addAttribute("page",session.getAttribute("page"));
+		mav.setViewName("redirect:/mbList");
+		return mav;
+	};
+	
+	@PostMapping("create")
+	public ModelAndView create(LoginVO loginVO, HttpServletRequest request,HttpServletResponse response) throws IOException {
+		ModelAndView mav = new ModelAndView();
+		HttpSession session = request.getSession();
+		String page = (String) session.getAttribute("page");
+		if(page == null)page = "1";
+		
+		Map<String, String> map = new HashMap();
+		map.put("mbId", loginVO.getId());
+		map.put("mbPw", loginVO.getPw());
+		
+		int dupleCheck = userService.mbDuplicationCheck(map);
+		System.out.println(dupleCheck);
+
+		if(dupleCheck > 0) {
+			String alertText = "중복이거나 유효하지 않은 접근입니다";
+			String redirectPath = "/main";
+			System.out.println(loginVO.getAdmin());
+			if(loginVO.getAdmin() != null) {
+				redirectPath = "/main/mbList?page="+page;
+			}
+			CommonUtils.redirect(alertText, redirectPath, response);
+		}else {
+			String IP = CommonUtils.getClientIP(request);
+			int totalcount = userService.mbGetAll();
+			LoginDomain loginDomain = LoginDomain.builder()
+					.mbId(loginVO.getId())
+					.mbPw(loginVO.getPw())
+					.mbLevel((totalcount == 0) ? "3" : "2")
+					.mbIp(IP)
+					.mbUse("Y")
+					.build();
+			userService.mbCreate(loginDomain);
+			
+			if(loginVO.getAdmin() == null) {
+				session.setAttribute("ip",IP);
+				session.setAttribute("id", loginDomain.getMbId());
+				session.setAttribute("mbLevel", (totalcount == 0) ? "3" : "2");
+				mav.setViewName("redirect:/bdList");
+			}else {
+				mav.setViewName("redirect:/mbList?page=1");
+			}
+		}
+		return mav;
+	};
+	
+	@GetMapping("signin")
+    public ModelAndView signIn() throws IOException {
+		ModelAndView mav = new ModelAndView();
+        mav.setViewName("signin/signin.html"); 
+        return mav;
+    };
+    
+    @RequestMapping("logout")
+	public ModelAndView logout(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		HttpSession session = request.getSession();
+		session.invalidate();
+		mav.setViewName("index.html");
+		return mav;
+	}
+	
+	
+	
 }
--- src/main/java/com/co/kr/util/Pagination.java
+++ src/main/java/com/co/kr/util/Pagination.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,8 @@ public class Pagination {
 	
 	public static Map<String,Object> pagination(int totalcount, HttpServletRequest request) {
 		Map<String,Object> map = new HashMap<String,Object>();
-		String pnum = request.getParameter("page");
+		HttpSession session = request.getSession();
+		String pnum = (String) session.getAttribute("page");
 		System.out.println("pnum"+pnum);
 		
 		if (pnum == null) { pnum = "1"; }
@@ -42,4 +44,36 @@ public class Pagination {
 
 		return map;
 	}
+	
+	public static Map<String,Object> uploadPagination(int Gtotal, int pageCount, HttpServletRequest request) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		String pnum = request.getParameter("Gnum");
+		if (pnum == null) { pnum = "1"; }
+		int rowNUM = Integer.parseInt(pnum);
+		if(rowNUM < 0) {rowNUM = 1;}
+		int pageNum;
+		if (Gtotal % pageCount == 0) { pageNum = Gtotal / pageCount; 
+		}else { 
+			pageNum = (Gtotal / pageCount) + 1; 
+			}
+	
+		if(rowNUM > pageNum) { rowNUM = pageNum; }
+		
+		int temp = (rowNUM - 1) % 10;
+		int startpage = rowNUM - temp;
+		int endpage = startpage + 9;
+		if (endpage > pageNum) { endpage = pageNum; }
+		int offset = (rowNUM - 1) * pageCount;
+		
+		map.put("rowNUM", rowNUM);
+		map.put("startpage", startpage);
+		map.put("endpage", endpage);
+		map.put("pageNum", pageNum);
+		map.put("offset", offset);
+
+		return map;
+	}
+	
+	
+	
 }
